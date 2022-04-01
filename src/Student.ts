6a193bd68eac378447efbf542eba9fcecf1bb037
ncardenas <nc.cardenas@hotmail.com>
2022-04-01 15:53:53 -0600

    Removed unused code in students.ts

--- src/Student.ts
+++ src/Student.ts
@@ -3,11 +3,6 @@ import Time from './Time'
 class Student {
     id: number
     grade: number
-    monday: Object[] = []
-    tuesday: Object[] = []
-    wednesday: Object[] = []
-    thursday: Object[] = []
-    friday: Object[] = []
     availability: Object
 
     constructor(student_id: number=0, grade: number=0) {
@@ -24,22 +19,10 @@ class Student {
     
     addAvailability(day: string, start: Time, end: Time) {
         this.availability[day.toLowerCase()].push( {'start': start, 'end': end} )
-        //if (day === 'monday') { this.monday.push(availability) }
-        //else if (day === 'tuesday') { this.tuesday.push(availability) }
-        //else if (day === 'wedesnday') { this.tuesday.push(availability) }
-        //else if (day === 'thursday') { this.tuesday.push(availability) }
-        //else if (day === 'friday') { this.tuesday.push(availability) }
-        //else {}
     }
 
     getAvailability(day: string) : Object[] {
         return this.availability[day.toLowerCase()]
-        //if (day === 'monday') { return this.monday }
-        //else if (day === 'tuesday') { return this.tuesday }
-        //else if (day === 'wednesday') { return this.wednesday }
-        //else if (day === 'thursday') { return this.thursday }
-        //else if (day === 'friday') { return this.friday }
-        //else {}
     }
 }
 
