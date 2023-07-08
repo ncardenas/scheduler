ebda6880dad3bc8287765a492bfaf77c3fafdbb8
ncardenas <nc.cardenas@hotmail.com>
2023-07-08 12:42:12 -0600

    Updates

---
+++ preload.js
@@ -0,0 +1,12 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("api", {
+  openDialog: (arg) => ipcRenderer.invoke("open-dialog", arg),
+  parseCSV: (arg) => ipcRenderer.invoke("parse-csv", arg)
+})
+
+contextBridge.exposeInMainWorld("myApp", {
+  sayHello: (arg) => ipcRenderer.invoke("say-hello", arg)
+})
